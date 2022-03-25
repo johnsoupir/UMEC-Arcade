810025e63a58f6c3590c4bea48ed3d396532d1ab
John Soupir <johnsoupir@gmail.com>
2022-03-24 19:17:16 -0500

    Added functions

--- Arduino/Arcade_Joystick.ino
+++ Arduino/Arcade_Joystick.ino
@@ -1,44 +1,17 @@
-/*
-  Analog Input
-
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
-
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This eyample code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInput
-*/
-
-int xPin = A1;    // select the input pin for the potentiometer
+int xPin = A1;    // pins for joystick pot
 int yPin = A2;    
 
+//Variable to store the position
 int xVal;
 int yVal;
 
-
+//Outputs
 int xUpPin = 13;
 int xDownPin = 7;
 int yUpPin = 8;
 int yDownPin = 9;
 
+//Setup
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(xUpPin,OUTPUT);
@@ -48,7 +21,7 @@ void setup() {
 }
 
 
-
+//Main loop
 void loop() {
 
   xVal=analogRead(xPin);
--- BASH-UI/ui.sh
+++ BASH-UI/ui.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 
-#OPTIONS=$(cat ./GamePaths.csv | awk -F, "{ print  "$1" }" ) 
-#NUMGAMES=$(seq 1 $(cat ./GamePaths.csv | awk -F, "{ print $1 }" | wc -l))
-#GAMES=$(cat ./GamePaths.csv | awk -F, '{ print $1 }' | sed 's/"//g')
+#Some vars
+range="[0-9]"
+
+#Build list of games
 seq 1 $(cat ./GamePaths.csv | awk -F, "{ print $1 }" | wc -l) > numGames.tmp
 cat ./GamePaths.csv | awk -F, '{ print $1 }' | sed 's/"//g' > games.tmp
 
-#paste numGames.tmp games.tmp
-#rm *.tmp
-#exit
 
-#input=0
-range="[0-9]"
-
-figlet "Umary Engineering Club"
-
+#Creating Functions for common things
+################################################################################################
+function banner {
 echo "--------------------------------------------------------------"
 echo "|            *                                 *             |"
 echo "|                        *                             *     |"
@@ -25,31 +21,45 @@ echo "|       *                         *                          |"
 echo "|                    *                      *                |"
 echo "|   *                          *                     *       |"
 echo "--------------------------------------------------------------"
+}
 
 
-echo "Here are the games available: "
+function get_highscore {
+echo "Blah"
+}
 
+
+function game_select {
 paste numGames.tmp games.tmp
-
-rm *.tmp
-
-
+#rm *.tmp
+echo "Here are the games available: "
 while  [[ 1 ]]; do
 	read input	
 	if [[ $input != $range ]]; then
 		echo "INVALID SELECTION!"
 	else
 		echo "Selected $input"
+		GAME=$()		
 		break
 	fi
 
 done
+}
+#END OF FUNCTIONS
+####################################################################################################
+
+
+#Main
+
+figlet "Umary Engineering Club"
+
+banner
+
+game_select
 
 
 
-#if ( $(echo $input | grep "^-\?[0-9]*$") == $input ); then
-#	echo $input
-#fi
 
-#cat ./GamePaths.csv | awk -F, '{ print $1 }'
+
+
 
